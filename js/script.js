6ddf231cc9ad222884dda1ef58b65664e4d66866
edwin24p <89165711+edwin24p@users.noreply.github.com>
2023-01-25 17:14:03 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+let toggleButton = document.querySelector(".header__hamburger")
+let links = document.querySelector(".header__links")
+let close = document.querySelector(".header__close")
+let overlay = document.querySelector('.header__overlay')
+let body = document.querySelector("body")
+let individualLink = document.querySelectorAll('.header__link')
+
+oggleButton.addEventListener('click', function(e){
+    overlay.classList.add('active');
+    links.classList.add('active');
+    close.classList.add('active');
+    body.classList.add('no-scroll');
+    
+});
+
+close.addEventListener('click', function(e){
+    overlay.classList.remove('active');
+    links.classList.remove('active');
+    close.classList.remove('active');
+    body.classList.remove('no-scroll');
+});
+
+overlay.addEventListener('click', function(e){
+    overlay.classList.remove('active');
+    links.classList.remove('active');
+    close.classList.remove('active');
+    body.classList.remove('no-scroll');
+})
+
+individualLink.forEach(link=>{
+    link.addEventListener('click', function(e){
+        if(window.innerWidth<=992){
+        overlay.classList.remove('active');
+        links.classList.remove('active');
+        close.classList.remove('active');
+        body.classList.remove('no-scroll');
+        }
+        
+    })
+})
